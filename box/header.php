88c98f60161bc5b032f69e6804ee9553a42507d9
shiv1810 <shivmistry978@gmail.com>
2024-10-13 01:39:20 +0530

    upload

---
+++ box/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <!--Meta Tags Starts Here-->
+        <meta charset="UTF-8" />
+        <meta name="author" content="Vijay Thapa - Online Examination System" />
+        <meta name="description" content="Test Preparation Portal by Vijay Thapa." />
+        <meta name="keywords" content="Test Preaparation, Online Portal, Online Exam System, Vijay Thapa, Kathmandu, Nepal" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <!--Meta Tags Ends Here-->
+        <title>Online Examination System - Vijay Thapa</title>
+        
+        <!--COUNT DOWN TIMER STARTS HERE-->
+        <script src="<?php echo SITEURL; ?>/assets/js/countdown/jquery.js"></script>
+        <script src="<?php echo SITEURL; ?>/assets/js/countdown/jquery.simple.timer.js"></script>
+        <script>
+          $(function(){
+            $('.timer').startTimer();
+          });
+        </script>
+        <!--COUNT DOWN TIMER ENDS HERE-->
+        
+        <!--ADDING CKEDITOR HERE-->
+        <script type="text/javascript" src="<?php echo SITEURL; ?>/assets/ckeditor/ckeditor.js"></script>
+        
+        <!--CSS File Starts Here-->
+        <link rel="stylesheet" type="text/css" href="<?php echo SITEURL; ?>/assets/css/style.css" />
+        <!--CSS File Ends Here-->
+    </head>
+    
+    <body>
+        <!--Header Starts Here-->
+        <header class="header">
+            <div class="wrapper clearfix">
+                <div class="logo">
+                    <img src="<?php echo SITEURL; ?>/images/logo.jpg" alt="Vijay Thapa" title="Online Examination System - Vijay Thapa" />
+                </div>
+                
+                <div class="head-title">
+                    <h1>Online Test Preparation Portal</h1>
+                </div>
+            </div>
+        </header>
+        <!--Header Ends Here-->
