de1bb162c54be302dc0dea15a535ba4376307583
Sush893 <suhani12345singh@gmail.com>
2024-10-14 11:41:36 +0530

    quess

--- box/footer.php
+++ box/footer.php
@@ -2,8 +2,8 @@
         <footer class="footer">
             <div class="wrapper">
                 <p>
-                    <a href="https://www.youtube.com/vijaythapa?sub_confirmation=1/" title="ABC Institutes">ABC Institute</a> &copy; <?php echo date('Y'); ?>. All Rights Reserved.    
-                    Powered by <a href="https://www.youtube.com/vijaythapa?sub_confirmation=1" title="Vijay Thapa" target="_blank">Vijay Thapa</a>.
+                    <a href="https://www.youtube.com/Ent?sub_confirmation=1/" title="Navrachana University">Navrachana University</a> &copy; <?php echo date('Y'); ?>. All Rights Reserved.    
+                    Powered by <a href="https://www.youtube.com/Ent?sub_confirmation=1" title="Ent_Project" target="_blank">Ent_Project</a>.
                 </p>
             </div>
         </footer>
